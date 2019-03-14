5b17efecd5b2ad3e2db172151a66b269ae668c9b
Ian Albuquerque <ian.albuquerque.silva@gmail.com>
2019-03-14 17:35:02 -0500

    player input script prototype

---
+++ Assets/Scripts/PlayerJointRotationInput.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJointRotationInput : MonoBehaviour
+{
+    public KeyCode key;
+
+    public Rigidbody2D targetRigidBody;
+
+    public float maxIntensity;
+
+    float direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(this.key)) {
+            Debug.Log("Pressed!");
+            this.targetRigidBody.AddTorque(this.maxIntensity);
+        }
+    }
+}
