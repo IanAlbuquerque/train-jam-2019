9cde1732157b32b6f5e32c1ecee63efdc5e7bad8
Parker Hamilton <PARKER.HAMILTON@iam.local>
2019-03-14 19:34:36 -0500

    Changed scene movement scripts

---
+++ Assets/Scripts/SceneMover.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneMover : MonoBehaviour
+{
+    [SerializeField]
+    private Button startButton;
+
+    [SerializeField]
+    private Button creditsButton;
+
+    [SerializeField]
+    private Button quitButton;
+
+    [SerializeField]
+    private Button nextOnInstructionsButton;
+
+    [SerializeField]
+    private Button returnToMainMenuButton;
+
+    
+
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene("BrawlScene");
+    }
+
+    public void LoadCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+}
+
+
