9cdf6d5faec4bbc0c8b7c8411a2e9c73bbfef165
Parker Hamilton <PARKER.HAMILTON@iam.local>
2019-03-14 21:31:22 -0500

    Created Simple Cam follow, also created simple out of bounds detection - but not working, mostly just..., created the script.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    Transform player1Target;
+
+    [SerializeField]
+    Transform player2Target;
+
+    [SerializeField]
+    public float smoothSpeed = 0.125f;
+
+    [SerializeField]
+    public Vector3 offset;
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = (player1Target.position - player2Target.position) + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        transform.LookAt(player1Target);
+    }
+}
